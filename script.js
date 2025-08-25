77ab61fb75f118e3d8973c220e7db822fc17a4d9
Alfishan Shaikh <shaikhalfishan2401@gmail.com>
2025-08-25 11:39:39 +0530

    script update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btn= document.getElementById("btn");
 const output = document.querySelector(".output");
 
 let quote = [
-   " Avoid daydreaming about the years to come.","You are the most important person in your whole life.","Always be true to who you are, and ignore what other people have to say about you.","Always be true to who you are, and ignore what other people have to say about you.","Only demonstrate your strength when it’s really required.","Subscribe to Drop X Out"
+   " Avoid daydreaming about the years to come.","You are the most important person in your whole life.","Always be true to who you are, and ignore what other people have to say about you.","Always be true to who you are, and ignore what other people have to say about you.","Only demonstrate your strength when it’s really required."
 ]
 
 btn.addEventListener("click",()=>{
